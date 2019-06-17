0ecca5a62e7a7c7009eb533e2033b1037f41dfa0
liangneng <liangneng@liangnengdeiMac.local>
2019-06-17 09:11:01 +0800

    Initial Commit

---
+++ Incubator_demo01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Incubator_demo01
+//
+//  Created by liangneng on 2019/6/17.
+//  Copyright © 2019 liangneng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Incubator_demo01Tests/Incubator_demo01Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Incubator_demo01Tests.swift
+//  Incubator_demo01Tests
+//
+//  Created by liangneng on 2019/6/17.
+//  Copyright © 2019 liangneng. All rights reserved.
+//
+
+import XCTest
+@testable import Incubator_demo01
+
+class Incubator_demo01Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
